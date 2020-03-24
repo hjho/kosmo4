11fdafa62f4cafffb7cf76c65ebed8c81004bd9f
hjho <gjwnsgh131@naver.com>
2020-03-24 18:53:52 +0900

    준호가 마스터에서 마스터 풀 이후 변경 이 후 커밋

--- kosmo4/src/master/MasterTest2.java
+++ kosmo4/src/master/MasterTest2.java
@@ -5,6 +5,8 @@ public class MasterTest2 {
 	public static void main(String[] args) {
 		//System.out.println("충식작업2");
 		System.out.println("충식작업3");
+		//준호가 마스터에서 마스터 풀 이후 변경
+		//이 후 커밋
 	}
 
 }
