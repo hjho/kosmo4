a0171e6077ee9321a5021118c46578ff0b20edba
kcs886 <rlacndtlr7482@gmail.com>
2020-03-24 16:30:55 +0900

    16:30

--- kosmo4/src/kcs/KcsTest3.java
+++ kosmo4/src/kcs/KcsTest3.java
@@ -5,6 +5,8 @@ public class KcsTest3 {
 	public static void main(String[] args) {
 		System.out.println("충식 작업 내용3");
 		System.out.println("충식 작업 내용4");
+		System.out.println("충식 작업 내용5");
+		System.out.println("충식 작업 내용6");
 	}
 
 }
