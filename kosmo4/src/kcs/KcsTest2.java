1e25a97f07ad92866417ee00ef5926bc8e5e0d67
kcs886 <rlacndtlr7482@gmail.com>
2020-03-24 16:17:22 +0900

    층식이 다시 바꿈 16:17

--- kosmo4/src/kcs/KcsTest2.java
+++ kosmo4/src/kcs/KcsTest2.java
@@ -4,6 +4,7 @@ public class KcsTest2 {
 
 	public static void main(String[] args) {
 		System.out.println("충식 작업 내용3");
+		System.out.println("충식 작업 내용4");
 	}
 
 }
--- kosmo4/src/master/MasterTest2.java
+++ kosmo4/src/master/MasterTest2.java
@@ -3,7 +3,8 @@ package master;
 public class MasterTest2 {
 
 	public static void main(String[] args) {
-		System.out.println("충식작업2");
+		//System.out.println("충식작업2");
+		System.out.println("충식작업3");
 	}
 
 }
